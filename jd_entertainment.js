47928ca3ef76908f522363c358e6af52e2944eda
Kyle <aw123e@126.com>
2021-02-19 16:13:08 +0800

    .

--- jd_entertainment.js
+++ jd_entertainment.js
@@ -1,7 +1,8 @@
 /*
 百变大咖秀
 活动入口：首页搜索-‘百变大咖秀’-底部最右侧按钮
-活动地址：https://lzdz-isv.isvjcloud.com/dingzhi/change/able/activity/3508994?activityId=dz2102100001340202
+请手动进入一次活动页面已确保能够自动抽奖
+活动地址：https://lzdz-isv.isvjcloud.com/dingzhi/change/able/activity/3508994?activityId=dz2102100001340203
 
 新手写脚本，难免有bug，能用且用。
 多谢 whyour 大佬 指导
@@ -9,7 +10,6 @@
 脚本内置了一个给作者任务助力的网络请求，默认开启，如介意请自行关闭。
 助力活动链接： https://h5.m.jd.com/babelDiy/Zeus/4ZK4ZpvoSreRB92RRo8bpJAQNoTq/index.html
 参数 helpAuthor = false
-
 
 更新地址：https://raw.githubusercontent.com/i-chenzhe/qx/main/jd_entertainment.js
 ============Quantumultx===============
@@ -30,17 +30,17 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
 let cookiesArr = [], cookie = '', originCookie = '', message = '';
 let helpAuthor = false;//为作者助力的开关
-const ACT_ID = 'dz2102100001340202';
+const ACT_ID = 'dz2102100001340203';
 const questionList = [
-  { q: '1b04f39bf2ee4953bc7aa7c2ba037b01', a: 'A:刘维' },
-  { q: '1d777fac821e4614b51314d0b035660e', a: 'C:魏大勋' },
-  { q: '4ddac4345a55479abb17f97476b84eb1', a: 'B:李斯丹妮' },
-  { q: '543885a6e9b845cc8d97e92fd19e17f3', a: 'B:瞿颖' },
-  { q: '8068df6cd3fa41dd8565ac847d551981', a: 'A:戚薇' },
-  { q: '8bcad350090c41adbe881c5ef3553a7f', a: 'A:王智' },
-  { q: 'd81508f5253b481f962e48d754d33485', a: 'C:孟佳' },
-  { q: 'e24cf162152e4d2bb2172d25a411d5f1', a: 'C:杨迪' },
-  { q: 'fd984e91df104844a0a22c46f4d774b7', a: 'B:吴昕' }
+  { q: '84d9ef3ccb2543449935871c2a95f2aa', a: 'B:玲花' },
+  { q: '12ffa15a77c3489ca644ff0880e79343', a: 'A:倪萍' },
+  { q: '2c71328d97e6422a8181c6c025e078cc', a: 'A:管乐' },
+  { q: '3e7c6a1f59e043a59bcdca9b01b4cdaa', a: 'B:汪小敏' },
+  { q: '3f471e4309d7435c81604d0fe1d600b7', a: 'A:王智' },
+  { q: '482efac942f64d52852e92acdf9843df', a: 'B:孙楠' },
+  { q: '66e57d12ab4b4a018a3c2f6a5ebf68ba', a: 'A:曾毅' },
+  { q: 'ac6c845f83844995b8501665f06fe845', a: 'B:朱婧汐' },
+  { q: 'f3c17cb462de4841a0693abdee2b7815', a: 'A:黄艺馨' }
 ]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
@@ -91,6 +91,8 @@ if ($.isNode()) {
     $.done();
   })
 async function entertainment() {
+
+  $.risk = false;
   $.gameScore = 0;
   await grantTokenKey();
   await $.wait(1500)
@@ -104,30 +106,41 @@ async function entertainment() {
   await $.wait(1500)
   await getUserInfo();
   await $.wait(1500)
-  await getActContent(false,$.userShareCode);
-  await $.wait(1500)
-  await getActContent($.doJob);
-  await $.wait(1500)
-  await answer();
-  await $.wait(1500)
-  await draw();
+  await getActContent(false, $.userShareCode);
+  if (!$.risk) {
+    await $.wait(1500)
+    await getActContent($.doJob);
+    await $.wait(1500)
+    await answer();
+    await $.wait(1500)
+    await getActContent(false);
+    await draw();
+    console.log(`好友助力码【 ${$.shareCode} 】`);
+    await submitShareCode({ 'share_code': $.shareCode, 'pt_key': $.UserName });
+    if ($.isNode()) {
+      await notify.sendNotify(`${$.name}运行完成`, `京东账号${$.index} ${$.nickName || $.UserName}\n${message}`);
+    } else {
+      await $.msg(`${$.name}运行完成`, message);
+    }
+  } else {
+    if ($.isNode()) {
+      await notify.sendNotify(`${$.name}运行完成`, `京东账号${$.index} ${$.nickName || $.UserName}\n京东说‘本活动与你无缘，请关注其他活动。’`);
+    } else {
+      await $.msg(`${$.name}运行完成`, `京东说‘本活动与你无缘，请关注其他活动。’`);
+    }
+  }
   if (helpAuthor) {
     new Promise(resolve => { $.get({ url: 'https://api.r2ray.com/jd.bargain/index' }, (err, resp, data) => { try { if (data) { $.dataGet = JSON.parse(data); if ($.dataGet.data.length !== 0) { let opt = { url: `https://api.m.jd.com/client.action`, headers: { 'Host': 'api.m.jd.com', 'Content-Type': 'application/x-www-form-urlencoded', 'Origin': 'https://h5.m.jd.com', 'Accept-Encoding': 'gzip, deflate, br', 'Cookie': cookie, 'Connection': 'keep-alive', 'Accept': 'application/json, text/plain, */*', 'User-Agent': 'jdapp;iPhone;9.4.0;14.3;;network/wifi;ADID/;supportApplePay/0;hasUPPay/0;hasOCPay/0;model/iPhone10,3;addressid/;supportBestPay/0;appBuild/167541;jdSupportDarkMode/0;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1', 'Referer': `https://h5.m.jd.com/babelDiy/Zeus/4ZK4ZpvoSreRB92RRo8bpJAQNoTq/index.html?serveId=wxe30973feca923229&actId=${$.dataGet.data[0].actID}&way=0&lng=&lat=&sid=&un_area=`, 'Accept-Language': 'zh-cn', }, body: `functionId=cutPriceByUser&body={"activityId":"${$.dataGet.data[0].actID}","userName":"","followShop":1,"shopId":${$.dataGet.data[0].actsID},"userPic":""}&client=wh5&clientVersion=1.0.0` }; return new Promise(resolve => { $.post(opt, (err, ersp, data) => { }) }); } } } catch (e) { console.log(e); } finally { resolve(); } }) })
-}
-  console.log(`好友助力码【 ${$.shareCode} 】`);
-  console.log(message);
-  await submitShareCode({ 'share_code': $.shareCode, 'pt_key': $.UserName });
-  if ($.isNode()) {
-  await notify.sendNotify(`${$.name}运行完成`, `京东账号${$.index} ${$.nickName || $.UserName}\n请手动打开领取奖品\nhttps://lzdz-isv.isvjcloud.com/dingzhi/change/able/activity/3508994?activityId=dz2102100001340202\n`);
   }
+
 }
 
 async function draw() {
   for (let i = 0; i < $.cardList.length; i++) {
     const card = $.cardList[i];
-    if (card.answer === true && card.draw === false) {
+    if (card.answer === true && card.draw === false && $.canDraw === true) {
       console.log(`开始抽奖`);
-      await doTask('dingzhi/change/able/startDraw',`activityId=${ACT_ID}&actorUuid=${$.shareCode}&pin=${escape($.secretPin)}&cardId=${card.uuid}`)
+      await doTask('dingzhi/change/able/startDraw', `activityId=${ACT_ID}&actorUuid=${$.shareCode}&pin=${encodeURIComponent($.secretPin)}&cardId=${card.uuid}`)
       await $.wait(1000);
     }
   }
@@ -171,6 +184,11 @@ async function getActContent(done = true, authorShareCode = '') {
           console.log(`${JSON.stringify(err)}`)
         } else {
           data = JSON.parse(data);
+          if (data.result === false) {
+            $.risk = true;
+            console.log(`京东说‘本活动与你无缘，请关注其他活动。’`);
+            return;
+          }
           $.cardScore = data.data.cardScore;
           $.shareCode = data.data.actorUuid;
           $.addSku = data.data.addSku;
@@ -180,7 +198,7 @@ async function getActContent(done = true, authorShareCode = '') {
             $.doJob = false;
             if (data.data.drawOrNo === false && data.data.canDrawBig === true) {
               console.log(`开始抽取最终大奖。`)
-              await doTask('dingzhi/change/able/startDrawBig',`activityId=${ACT_ID}&actorUuid=${$.shareCode}&pin=${escape($.secretPin)}&cardId=`)
+              await doTask('dingzhi/change/able/startDrawBig', `activityId=${ACT_ID}&actorUuid=${$.shareCode}&pin=${escape($.secretPin)}&cardId=`)
             }
           }
           if (done) {
@@ -188,7 +206,7 @@ async function getActContent(done = true, authorShareCode = '') {
               let task = data.data[i];
               for (let vo of task.settings) {
                 let body1 = `activityId=${ACT_ID}&actorUuid=${$.shareCode}&pin=${encodeURIComponent($.secretPin)}&taskType=${vo.type}&taskValue=${vo.value}`;
-                let body2 = `venderId=${data.data.shopId}&elementId=${encodeURIComponent('店铺'+vo.value)}&pageId=${ACT_ID}&pin=${encodeURIComponent($.secretPin)}`;
+                let body2 = `venderId=${data.data.shopId}&elementId=${encodeURIComponent('店铺' + vo.value)}&pageId=${ACT_ID}&pin=${encodeURIComponent($.secretPin)}`;
                 if (vo.type === 12) {
                   console.log(`浏览会场 - ${vo.name}`)
                   await doTask('dingzhi/change/able/saveTask', body1);
@@ -240,9 +258,15 @@ function doTask(function_name, body) {
                 console.log(`回答正确。`)
               }
             }
-            if (data.data.hasOwnProperty('drawInfo') && data.data.drawInfo !== null) {
-              message += `获得${data.data.drawInfo.name}\n`
-              console.log(`获得${data.data.drawInfo.name}\n`);
+            if (data.data.hasOwnProperty('drawInfo')) {
+              if (data.data.drawOk === true) {
+                message += `获得${data.data.drawInfo.name}\n`
+                console.log(`获得${data.data.drawInfo.name}\n`);
+              } else {
+                $.canDraw = false;
+                console.log(`抽奖结果:${data.errorMessage}`);
+                $.msg(`${$.name}\n请手动进入一次活动页面后重新尝试`);
+              }
             }
           } else {
             console.log(data.errorMessage)
@@ -258,26 +282,26 @@ function doTask(function_name, body) {
 }
 function getAuthorCode(type) {
   return new Promise(async resolve => {
-      $.get({ url: `https://api.r2ray.com/jd.shareCode/author?type=${type}` }, (err, resp, data) => {
-          try {
-              if (err) {
-                  console.log(`${JSON.stringify(err)}`)
-              } else {
-                  if (data) {
-                      data = JSON.parse(data);
-                      if (data.data.length > 0 ) {
-                        $.authorShareCode = data.data[0].share_code
-                      }else{
-                        $.authorShareCode = '';
-                      }
-                  }
-              }
-          } catch (e) {
-              $.logErr(e, resp)
-          } finally {
-              resolve(data);
+    $.get({ url: `https://api.r2ray.com/jd.shareCode/author?type=${type}` }, (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+        } else {
+          if (data) {
+            data = JSON.parse(data);
+            if (data.data.length > 0) {
+              $.authorShareCode = data.data[0].share_code
+            } else {
+              $.authorShareCode = '';
+            }
           }
-      })
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve(data);
+      }
+    })
   })
 }
 function getUserInfo() {
@@ -295,7 +319,7 @@ function getUserInfo() {
             $.pinImg = data.data.yunMidImageUrl
             $.nickName = data.data.nickame
           } else {
-            console.log(data)
+            console.log(data);
           }
         }
       } catch (e) {
@@ -360,7 +384,7 @@ function grantTokenKey() {
       'Accept-Language': 'zh-Hans-CN;q=1',
       'Accept-Encoding': 'gzip, deflate, br',
     },
-    body: `body=%7B%22to%22%3A%22https%3A%5C%2F%5C%2Flzdz-isv.isvjcloud.com%5C%2Fdingzhi%5C%2Fchange%5C%2Fable%5C%2Factivity%3FactivityId%3Ddz2102100001340202%22%2C%22action%22%3A%22to%22%7D&build=167538&client=apple&clientVersion=9.3.8&openudid=b9b73293715e562295c0f0aac9d15035ea9b4889&sign=55a872906641d1ed946a1ba3458ebee9&st=1612496164952&sv=110`
+    body: `body=%7B%22to%22%3A%22https%3A%5C%2F%5C%2Flzdz-isv.isvjcloud.com%5C%2Fdingzhi%5C%2Fchange%5C%2Fable%5C%2Factivity%3FactivityId%3Ddz2102100001340203%22%2C%22action%22%3A%22to%22%7D&build=167538&client=apple&clientVersion=9.3.8&openudid=b9b73293715e562295c0f0aac9d15035ea9b4889&sign=55a872906641d1ed946a1ba3458ebee9&st=1612496164952&sv=110`
   }
   return new Promise(resolve => {
     $.post(opt, (err, resp, data) => {
@@ -377,7 +401,7 @@ function grantTokenKey() {
           }
         }
       } catch (e) {
-        console.log(e,resp)
+        console.log(e, resp)
       } finally {
         resolve();
       }
@@ -479,52 +503,52 @@ function taskPostUrl(function_id, body) {
 }
 function getShareCode() {
   return new Promise(async resolve => {
-      $.get({ url: `https://api.r2ray.com/jd.entertainment/index` }, (err, resp, data) => {
-          try {
-              if (err) {
-                  console.log(`${JSON.stringify(err)}`)
-              } else {
-                  if (data) {
-                    data = JSON.parse(data)
-                    if (data.data.length > 0 ) {
-                      $.userShareCode = data.data[0].share_code
-                    }else{
-                      $.userShareCode = '';
-                    }
-                  }
-              }
-          } catch (e) {
-              $.logErr(e, resp)
-          } finally {
-              resolve(data);
+    $.get({ url: `https://api.r2ray.com/jd.entertainment/index` }, (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+        } else {
+          if (data) {
+            data = JSON.parse(data)
+            if (data.data.length > 0) {
+              $.userShareCode = data.data[0].share_code
+            } else {
+              $.userShareCode = '';
+            }
           }
-      })
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve(data);
+      }
+    })
   })
 }
 function submitShareCode(body) {
   let opt = {
-      'url': `https://api.r2ray.com/jd.entertainment/update`,
-      'headers': {
-          "Content-Type": "application/json",
-      },
-      'body': JSON.stringify(body)
+    'url': `https://api.r2ray.com/jd.entertainment/update`,
+    'headers': {
+      "Content-Type": "application/json",
+    },
+    'body': JSON.stringify(body)
   }
   return new Promise(async resolve => {
-      $.post(opt, (err, resp, data) => {
-          try {
-              if (err) {
-                  console.log(`${JSON.stringify(err)}`)
-                  console.log(`${$.name} API请求失败，请检查网路重试`)
-              } else {
-                  data = JSON.parse(data);
-                  console.log(data.msg)
-              }
-          } catch (e) {
-              $.logErr(e, resp)
-          } finally {
-              resolve(data);
-          }
-      })
+    $.post(opt, (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} API请求失败，请检查网路重试`)
+        } else {
+          data = JSON.parse(data);
+          console.log(data.msg)
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve(data);
+      }
+    })
   })
 }
 function TotalBean() {
